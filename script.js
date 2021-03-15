9d609eb074b13be835c759c1ce8284b346243765
Michael Parenti <78945650+M-Coder-3920@users.noreply.github.com>
2021-03-15 10:08:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("one").onmouseover = function() {
+    document.querySelector("#background").style.backgroundImage = "url('https://images.unsplash.com/photo-1470219556762-1771e7f9427d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=889&q=80')";
+}
+document.getElementById("two").onmouseover = function() {
+    document.querySelector("#background").style.backgroundImage = "url('https://images.unsplash.com/photo-1514565131-fce0801e5785?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=824&q=80')";
+}
+document.getElementById("three").onmouseover = function() {
+    document.querySelector("#background").style.backgroundImage = "url('https://images.unsplash.com/photo-1524055908996-cd5183870b5d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80')";
+}
+document.getElementById("four").onmouseover = function() {
+    document.querySelector("#background").style.backgroundImage = "url('https://images.unsplash.com/photo-1493540447904-49763eecf55f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')";
+}
